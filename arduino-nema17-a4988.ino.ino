703e7ab81b843130a2a197d28e2c8feb88a2986b
João D'Amorim <joao.damorim@gmail.com>
2022-01-25 11:06:18 -0300

    Controle do motor nema 17, usando o driver a4988 em modo full-step.

---
+++ arduino-nema17-a4988.ino.ino
@@ -0,0 +1,28 @@
+int pins[] = {13,12,8,9,10,11}, velocidade;
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(pins[0], OUTPUT);
+  pinMode(pins[1], OUTPUT);
+  pinMode(pins[2], OUTPUT);
+  pinMode(pins[3], OUTPUT);
+  pinMode(pins[4], OUTPUT);
+  pinMode(pins[5], OUTPUT);
+  
+}
+
+void loop() {
+
+  //velocidade = 20;
+  //digitalWrite(pins[2], HIGH); 
+  digitalWrite(pins[3], LOW); 
+  digitalWrite(pins[4], LOW); 
+  digitalWrite(pins[5], LOW); 
+
+  digitalWrite(pins[0],HIGH); // Defini a direção de rotação
+  digitalWrite(pins[1],HIGH); // Envia um pulso de um passo
+  delay(5); 
+  digitalWrite(pins[1],LOW);
+ 
+}
